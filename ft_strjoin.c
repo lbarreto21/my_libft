44a3f461647cc59eab563101fc63abe03a408f92
lbarreto <luizfernandobarretodasilva@gmail.com>
2025-01-11 13:40:03 -0300

    Makefile fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lbarreto <lbarreto@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 15:41:26 by lbarreto          #+#    #+#             */
-/*   Updated: 2024/12/12 20:06:45 by lbarreto         ###   ########.fr       */
+/*   Updated: 2025/01/07 16:09:43 by lbarreto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	ft_strlcpy(join + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 	join[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	if (s1 != NULL)
-		free(s1);
+	free(s1);
 	free(s2);
 	return (join);
 }
